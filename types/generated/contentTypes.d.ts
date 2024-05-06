129a51ab9ab729a1278938c5c3e80e1057eb03dd
Nick <nicolachiarappa2001@gmail.com>
2024-05-06 18:50:45 +0200

    test

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,42 +362,6 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiArticoloArticolo extends Schema.CollectionType {
-  collectionName: 'articolos';
-  info: {
-    singularName: 'articolo';
-    pluralName: 'articolos';
-    displayName: 'Articolo';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    Titolo: Attribute.String;
-    Cover: Attribute.Media;
-    Paragrafo: Attribute.Component<'sezioni.paragrafo', true>;
-    Data: Attribute.Date;
-    slug: Attribute.UID<'api::articolo.articolo', 'Titolo'>;
-    Autore: Attribute.String;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::articolo.articolo',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::articolo.articolo',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -824,6 +788,64 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
+export interface ApiArticoloArticolo extends Schema.CollectionType {
+  collectionName: 'articolos';
+  info: {
+    singularName: 'articolo';
+    pluralName: 'articolos';
+    displayName: 'Articolo';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Titolo: Attribute.String;
+    Cover: Attribute.Media;
+    Paragrafo: Attribute.Component<'sezioni.paragrafo', true>;
+    Data: Attribute.Date;
+    slug: Attribute.UID<'api::articolo.articolo', 'Titolo'>;
+    Autore: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::articolo.articolo',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::articolo.articolo',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiTestTest extends Schema.CollectionType {
+  collectionName: 'tests';
+  info: {
+    singularName: 'test';
+    pluralName: 'tests';
+    displayName: 'Test';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    test: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::test.test', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::test.test', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -834,7 +856,6 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::articolo.articolo': ApiArticoloArticolo;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
@@ -843,6 +864,8 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
+      'api::articolo.articolo': ApiArticoloArticolo;
+      'api::test.test': ApiTestTest;
     }
   }
 }
